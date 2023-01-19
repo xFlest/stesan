7fd7e8736cd06ae5e66b3d4605894defb7ebe5b1
xFlest <mail@fle.st>
2023-01-20 00:30:55 +0900

    setup

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+const {src, dest} = require("gulp")
+const sass = require("gulp-sass")
+const pug = require("gulp-pug")
+
+const paths = {
+  'root': '.',
+  'scss': 'src/style/',
+  'pug': 'src/pug/',
+  'css': 'dist/css/',
+  'html': 'dist/'
+}
+
+gulp.task('build', ['pug', 'scss'])
+
+gulp.task('pug', () => {
+  return src(paths.pug + '**/*.pug').pipe(pug({pretty: true})).pipe(dest(paths.html))
+})
+gulp.task('sass', () => {
+  return src(paths.scss + '**/*.scss').pipe(sass({outputStyle: 'expanded'})).pipe(dest(paths.css))
+})
