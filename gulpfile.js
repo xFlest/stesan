4a6c140bdad9d97d16c1709715c0d2de151bdd1a
xFlest <mail@fle.st>
2023-01-22 02:22:29 +0900

    munyaaaaaa

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,21 @@
 const gulp = require("gulp")
 const sass = require("gulp-sass")(require('sass'))
 const pug = require("gulp-pug")
+const browserSync = require("browser-sync")  
 
 const paths = {
-  'root': '.',
-  'scss': 'src/scss/',
-  'pug': 'src/pug/',
-  'css': 'dist/css/',
-  'html': 'dist/'
+  'root': '/dist/',
+  'scss': '/src/scss/',
+  'pug': '/src/pug/',
+  'style': '/dist/css/',
 }
 
 
 gulp.task('pug', () => {
-  return gulp.src(paths.pug + '**/*.pug').pipe(pug({pretty: true})).pipe(gulp.dest(paths.html))
+  return gulp.src(paths.pug + '**/*.pug').pipe(pug({pretty: true})).pipe(gulp.dest(paths.root))
 })
 gulp.task('sass', () => {
-  return gulp.src(paths.scss + '**/*.scss').pipe(sass({outputStyle: 'expanded'})).pipe(gulp.dest(paths.css))
+  return gulp.src(paths.scss + '**/*.scss').pipe(sass({outputStyle: 'expanded'})).pipe(gulp.dest(paths.style))
 })
 
 
