86ec4dcb9b699f01c1dba0f1b89f8537b2f2baa8
mail@fle.st <mail@fle.st>
2023-01-22 11:44:16 +0900

    nya

--- gulpfile.js
+++ gulpfile.js
@@ -8,8 +8,6 @@ const paths = {
   'scss': 'src/scss/',
   'pug': 'src/pug/',
   'ts': 'src/ts/',
-  'style': 'dist/css/',
-  'script': 'dist/script/'
 }
 
 
@@ -17,10 +15,10 @@ gulp.task('pug', () => {
   return gulp.src(paths.pug + '**/*.pug').pipe(pug({pretty: true})).pipe(gulp.dest(paths.root))
 })
 gulp.task('sass', () => {
-  return gulp.src(paths.scss + '**/*.scss').pipe(sass({outputStyle: 'expanded'})).pipe(gulp.dest(paths.style))
+  return gulp.src(paths.scss + '**/*.scss').pipe(sass({outputStyle: 'expanded'})).pipe(gulp.dest(paths.root))
 })
 gulp.task('ts', () => {
-  return gulp.src(paths.ts + '**/*.ts').pipe(typescript()).pipe(gulp.dest(paths.script))
+  return gulp.src(paths.ts + '**/*.ts').pipe(typescript()).pipe(gulp.dest(paths.root))
 })
 
 
